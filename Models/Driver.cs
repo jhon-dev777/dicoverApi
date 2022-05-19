9d116c1cbfe9dcc116bf920fa6f85c230e990694
jhon-dev777 <jhonatan.condarco.developer@gmail.com>
2022-05-19 18:09:29 -0400

    DiscoverApi Feature | add proyect to VS 2022

---
+++ Models/Driver.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace DiscoverApi.Models;
+public class Driver
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public bool Status { get; set; }
+    public string? Description { get; set; }
+    public  string? Category { get; set; }
+    public int Rating { get; set; }
+    public ArraySegment<string> Routes { get; set; }
+    public User User { get; set; }
+}
